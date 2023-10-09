51d487c02dfd6bb6d012e3e10bdeee5b6a5bac6a
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-10-10 04:18:16 +0600

    Configure Ant Design and Basic Routing

---
+++ src/app/(withlayout)/student/page.tsx
@@ -0,0 +1,9 @@
+const StudentPage = () => {
+  return (
+    <div>
+      <h1>This is for student page</h1>
+    </div>
+  );
+};
+
+export default StudentPage;
---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+"use client"
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Somthing went wrong</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
